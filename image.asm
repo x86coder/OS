c3f73a1320f68bb276126f11ddd9f7b3c7a7d6eb
Amed Reyes <reyesrodriguezjaime@gmail.com>
2025-09-09 21:00:29 -0600

    stack

--- image.asm
+++ image.asm
@@ -1,34 +1,70 @@
-mov bx, 0000h
-mov dx, 0000h
-mov cx, 0001h
-mov ax, 0A3Ah
-int 10h
-inc dx
-mov ax, 0200h
-int 10h
-mov ax, 0A48h
-int 10h
-inc dx
-mov ax, 0200h
-int 10h
-mov ax, 0A65h
-int 10h
-inc dx
-mov ax, 0200h
-int 10h
-mov ax, 0A6Ch
-int 10h
-inc dx
-mov ax, 0200h
-int 10h
-mov ax, 0A6Ch
-int 10h
-inc dx
-mov ax, 0200h
-int 10h
-mov ax, 0A6Fh
-int 10h
-inc dx
-mov ax, 0200h
-int 10h
+mov ax, cs
+mov ss, ax
+mov sp, 01FCh
+mov ax, 0400h
 nop
+nop
+
+mov bx, 000Eh
+mov dx, 0001h
+mov cx, 0001h
+mov ax, 024Fh
+int 10h
+mov ah, 09h
+int 10h
+inc dx
+mov ah, 02h
+int 10h
+mov ax, 0953h
+int 10h
+inc dx
+inc dx
+mov ah, 02h
+int 10h
+mov ax, 092Fh
+int 10h
+inc dx
+inc dx
+mov ah, 02h
+int 10h
+
+mov ax, 0963h
+int 10h
+inc dx
+mov ah, 02h
+int 10h
+mov ax, 0973h
+int 10h
+inc dx
+mov ah, 02h
+int 10h
+
+mov ax, 093Ah
+int 10h
+inc dx
+mov ah, 02h
+int 10h
+
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+
+mov dx, 0100h
+mov ah, 02h
+int 10h
+mov ax, 093Ah
+int 10h
+
+loop:
+	pause
+jmp loop
+
+01E0h:
+	ret
