2eded9367cffefe0564d2748e26652dbd12cff7c
Amed Reyes <reyesrodriguezjaime@gmail.com>
2025-09-09 18:46:09 -0600

    code

---
+++ image.asm
@@ -0,0 +1,34 @@
+mov bx, 0000h
+mov dx, 0000h
+mov cx, 0001h
+mov ax, 0A3Ah
+int 10h
+inc dx
+mov ax, 0200h
+int 10h
+mov ax, 0A48h
+int 10h
+inc dx
+mov ax, 0200h
+int 10h
+mov ax, 0A65h
+int 10h
+inc dx
+mov ax, 0200h
+int 10h
+mov ax, 0A6Ch
+int 10h
+inc dx
+mov ax, 0200h
+int 10h
+mov ax, 0A6Ch
+int 10h
+inc dx
+mov ax, 0200h
+int 10h
+mov ax, 0A6Fh
+int 10h
+inc dx
+mov ax, 0200h
+int 10h
+nop
